4bca664bda45241f99b398b2d289e8fe10d6ed33
David Velasco <dvelasco@dhcp-10-105-230-174.wireless.northwestern.private>
2019-07-08 13:27:17 -0500

    New graphing file

---
+++ python_plot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 13:20:10 2019
+
+@author: dvelasco
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0,2*np.pi,100)
+plt.plot(t, np.sin(t), label="David's Graph")
+plt.xlabel("t [radians]")
+plt.ylabel("sin(t)")
+plt.xlim(0,2*np.pi)
+plt.legend()
+plt.show()
